d4b88cc3a1030b7f573d8087606383a0eb7898d9
y1s3m0 <unconfigured@null.spigotmc.org>
2022-04-06 23:30:28 +0800

    fix

--- start.sh
+++ start.sh
@@ -59,7 +59,7 @@ do
 	done	
 	echo "[$(date +%Y-%m-%d\ %H:%M:%S)]：结束脚本"
 
-	while [ "$(expr $(date +%s) - $(date +%s -d ${start_time}))" -lt 21600  ]
+	while [ $((($(date +%s) - $(date +%s -d "${start_time}"))/60)) -lt 3600  ]
 	do
 		sleep 600s
 	done
